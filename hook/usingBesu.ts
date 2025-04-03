9d4f7de5dbdba66300d241c5fadff1471e39c0b3
LaRojaPlamya162 <124785235+LaRojaPlamya162@users.noreply.github.com>
2025-04-03 17:55:00 +0700

    Update usingBesu.ts

--- hook/usingBesu.ts
+++ hook/usingBesu.ts
@@ -4,8 +4,8 @@ import { isBesuOnline } from "@/context/besuUtils";
 import { useBesu } from "@/context/besu-provider";
 
 const DIDRegistryABI = DIDRegistryArtifact.abi;
-const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // Cập nhật địa chỉ contract
-
+//const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // Cập nhật địa chỉ contract
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 export function usingBesu() {
   const { provider, account, setProvider, setAccount } = useBesu();
 
